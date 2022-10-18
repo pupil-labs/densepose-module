947ac1fad3476099b91f6ff11264d1470b92ddef
Miguel García García <mikelgg93@gmail.com>
2022-10-18 13:48:18 +0200

    Fix importlib_metadata name

--- src/pupil_labs/dense_pose/__init__.py
+++ src/pupil_labs/dense_pose/__init__.py
@@ -1,9 +1,9 @@
-"""Top-level entry-point for the <dynamic_content_on_rim> package"""
+"""Top-level entry-point for the <pl-densepose> package"""
 
 from importlib.metadata import PackageNotFoundError, version
 
 try:
-    __version__ = version("pupil_labs_dense_pose")
+    __version__ = version("pupil-labs-dense-pose")
 except PackageNotFoundError:
     # package is not installed
     pass
